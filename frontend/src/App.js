5affffa62b24a7e2c1a77ee2b3f5f9cf18a82ef8
Abhishek Lale <abhisheklale181@gmail.com>
2023-06-27 22:04:35 +0530

    Added views for drivers and cabs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Routes ,Route } from 'react-router-dom';
 import PageLayout from './components/layout/pagelayout'
@@ -7,21 +8,25 @@ import Drivers from './components/drivers/drivers';
 import Cabs from './components/cabs/cabs';
 import AddDriver from './components/drivers/adddriver';
 import AddCab from './components/cabs/addcab';
+import UpdateDriver from './components/drivers/updatedriver';
+import UpdateCab from './components/cabs/updateCab';
 
 function App() {
   return (
     <div className="app">
-      <Router>
-        <PageLayout>
-        <Routes>
-            <Route exact path="/" element={<Home />}/>
-            <Route exact path="/drivers" element={<Drivers />}/>
-            <Route exact path="/cabs" element={<Cabs />}/>
-            <Route exact path="/addDriver" element={<AddDriver />}/>
-            <Route exact path="/addCab" element={<AddCab />}/>
-        </Routes>
-        </PageLayout>
-      </Router>
+        <Router>
+          <PageLayout>
+          <Routes>
+              <Route exact path="/" element={<Home />}/>
+              <Route exact path="/drivers" element={<Drivers />}/>
+              <Route exact path="/cabs" element={<Cabs />}/>
+              <Route exact path="/addDriver" element={<AddDriver />}/>
+              <Route exact path="/addCab" element={<AddCab />}/>
+              <Route exact path="/updateDriver/:id" element={<UpdateDriver />}/>
+              <Route exact path="/updateCab/:id" element={<UpdateCab />}/>
+          </Routes>
+          </PageLayout>
+        </Router>
     </div>
   );
 }
