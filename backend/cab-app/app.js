f6b48d89cc8688fc663ed3275f69b4623c972db1
Tejas Mane <tejasmane485@gmail.com>
2023-07-03 13:14:46 +0530

    jwt authentication added

--- backend/cab-app/app.js
+++ backend/cab-app/app.js
@@ -16,6 +16,25 @@ app.use(express.json())
 app.use(cors("*"))
 app.use(morgan("combined"))
 
+// Middleware function for user authentication and authorization
+function authorizeUser(req, res, next) {
+  const token = req.headers["token"]
+
+  if (!token) {
+    res.status(401).json({ message: "Invalid token" })
+  } else {
+    try {
+      const data = jwt.verify(token, process.env.JWT_SECRET)
+      req.id = data.id
+      next()
+    } catch (exception) {
+      res.status(401).json({ message: "Invalid token" })
+    }
+  }
+}
+
+app.use(authorizeUser)
+
 // Routes
 app.use("/cabs", cabRoutes)
 
