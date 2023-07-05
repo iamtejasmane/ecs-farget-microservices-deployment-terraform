3ccf42ca4a5a193de8a9ebae6a9dd18eee913b08
Tejas Mane <tejasmane485@gmail.com>
2023-07-05 23:02:42 +0530

    ports updated for backend

--- backend/cab-app/app.js
+++ backend/cab-app/app.js
@@ -39,7 +39,7 @@ app.use(authorizeUser)
 app.use("/cabs", cabRoutes)
 
 // Start the server
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`.green)
 })
