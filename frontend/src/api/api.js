7b560a56e5da080e941da062fa1bd23898407a89
Tejas Mane <tejasmane485@gmail.com>
2023-07-12 09:30:20 +0530

    env variable change

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
-const cabAppUrl = process.env.CAB_APP || "http://localhost:4000"
-const driverAppUrl = process.env.DRIVER_APP || "http://localhost:5000"
+const cabAppUrl = process.env.REACT_APP_CAB_API_URL || "http://localhost:4000"
+const driverAppUrl =
+  process.env.REACT_APP_DRIVER_API_URL || "http://localhost:5000"
 const cabDriverAppUrl =
-  process.env.CAB_ASSIGNMENT_APP || "http://localhost:6000"
-
+  process.env.REACT_APP_CAB_ASSIGN_API_URL || "http://localhost:6000"
 
 export const loginOwner = (data) => {
   return axios.post(`${driverAppUrl}/owner/login`, data)
-}  
+}
 
 export const fetchDrivers = () => {
   return axios.get(`${driverAppUrl}/drivers`)
@@ -30,26 +30,25 @@ export const deleteDriver = (id) => {
   return axios.delete(`${driverAppUrl}/drivers/${id}`)
 }
 
-
 export const fetchCabs = () => {
-    return axios.get(`${cabAppUrl}/cabs`)
+  return axios.get(`${cabAppUrl}/cabs`)
 }
 
 export const fetchCab = (id) => {
-    console.log(`${cabAppUrl}/cabs/${id}`)
-    return axios.get(`${cabAppUrl}/cabs/${id}`)
+  console.log(`${cabAppUrl}/cabs/${id}`)
+  return axios.get(`${cabAppUrl}/cabs/${id}`)
 }
 
 export const createCab = (data) => {
-    return axios.post(`${cabAppUrl}/cabs`, data)
+  return axios.post(`${cabAppUrl}/cabs`, data)
 }
 
-export const updateCab = (id,data) => {
-    return axios.put(`${cabAppUrl}/cabs/${id}`, data)
+export const updateCab = (id, data) => {
+  return axios.put(`${cabAppUrl}/cabs/${id}`, data)
 }
 
 export const deleteCab = (id) => {
-    return axios.delete(`${cabAppUrl}/cabs/${id}`)
+  return axios.delete(`${cabAppUrl}/cabs/${id}`)
 }
 
 export const getCabDrivers = () => {
@@ -60,7 +59,7 @@ export const assignCabDriver = (data) => {
   return axios.get(`${cabDriverAppUrl}/assignments`, data)
 }
 
-export const updateCabDriverAssignment = (id,data) => {
+export const updateCabDriverAssignment = (id, data) => {
   return axios.get(`${cabDriverAppUrl}/assignments/${id}`, data)
 }
 
